1f0f19be8007371994dee97bb3a75a07be68b98f
Yang Qi <yangqi137@me.com>
2016-12-07 17:08:35 -0500

    working on the new interface.

---
+++ src/yqvmc/bin_collector.hpp
@@ -0,0 +1,53 @@
+#ifndef YQVMC_BIN_COLLECTOR_PLAIN_HPP
+#define YQVMC_BIN_COLLECTOR_PLAIN_HPP
+
+#include <tuple>
+#include <iostream>
+
+#include <boost/accumulators/accumulators.hpp>
+#include <boost/accumulators/statistics/stats.hpp>
+#include <boost/accumulators/statistics/mean.hpp>
+#include <boost/accumulators/statistics/error_of.hpp>
+#include <boost/accumulators/statistics/error_of_mean.hpp>
+
+namespace yqvmc {
+  template <class M, typename EnableCompositeResult = void>
+  class BinCollectorPlain {
+  public:
+    typedef M Measure;
+    typedef typename M::result_type result_type;
+
+    BinCollectorPlain(std::string name, Measure& measure)
+    : m_name(name), m_measure(measure) {}
+
+    void collect(std::size_t iBin) {
+      auto result = m_measure.result();
+      m_acc(result);
+      m_acc2(result * result);
+      std::cout << m_name << ": " << result << std::endl;
+    }
+
+    void report() const {
+      auto sum = boost::accumulators::sum(m_acc);
+      auto sum2 = boost::accumulators::sum(m_acc2);
+      double count = boost::accumulators::count(m_acc);
+      auto mean = sum / count;
+      auto error = sum2 / count - mean*mean;
+      std::cout << m_name << " : " << mean
+                << " +/- " << error
+                << std::endl;
+    }
+
+  private:
+    std::string m_name;
+    Measure& m_measure;
+    typedef boost::accumulators::tag::sum tag_sum;
+    typedef boost::accumulators::tag::count tag_count;
+    typedef boost::accumulators::stats<tag_sum, tag_count> stats;
+    boost::accumulators::accumulator_set<result_type, stats> m_acc;
+    boost::accumulators::accumulator_set<result_type, stats> m_acc2;
+  };
+
+}
+
+#endif
--- src/yqvmc/engine.hpp
+++ src/yqvmc/engine.hpp
@@ -6,62 +6,44 @@
 namespace yqvmc {
   class Engine {
   public:
-    std::size_t warmup;
-    std::size_t skip;
-    std::size_t n;
+    std::size_t m_warmup;
+    std::size_t m_skip;
+    std::size_t m_binsize;
+    std::size_t m_nbins;
 
   public:
-    template <typename Configuration, 
+    template <typename Configuration,
 	      typename Walker,
 	      typename RG,
+        typename BinCollector,
 	      typename... Measures>
-    void run(Configuration& conf, Walker& walker, 
-	     RG& g, Measures&... measures) const {
-      if (warmup>0)
-          for (std::size_t i=0; i<warmup; i++)
+    void run(Configuration& conf, Walker& walker, RG& g,
+             BinCollector& binCol, Measures&... measures) const {
+      if (m_warmup>0)
+          for (std::size_t i=0; i<m_warmup; i++)
               walker.walk(conf, g);
 
-      for (std::size_t i=0; i<n; i++) {
-          measure(conf, measures...);
+      for (std::size_t ibin = 0; ibin < m_nbins; ibin++) {
+        for (std::size_t i = 0; i < m_binsize; i++) {
+          measure(conf, i, measures...);
           walker.walk(conf, g);
-		  
-          if (skip>0)
-              for (std::size_t j=0; j<skip; j++)
-                  walker.walk(conf, g);
-      }
-    }
-
-    template <typename Configuration, 
-	      typename Walker1,
-	      typename Walker2,
-	      typename RG,
-	      typename... Measures>
-    void run_warmupWalker(Configuration& conf, 
-			  Walker1& walker1, Walker2& walker2,
-			  RG& g, Measures&... measures) const {
-      if (warmup>0)
-          for (std::size_t i=0; i<warmup; i++)
-              walker1.walk(conf, g);
-
-      for (std::size_t i=0; i<n; i++) {
-          measure(conf, measures...);
-          walker2.walk(conf, g);
-		  
-          if (skip>0)
-              for (std::size_t j=0; j<skip; j++)
-                  walker2.walk(conf, g);
+          if (m_skip > 0)
+            for (std::size_t j=0; j < m_skip; j++)
+              walker.walk(conf, g);
+        }
+        binCol.collect(ibin);
       }
     }
 
   private:
     template <typename Configuration>
-    static void measure(const Configuration& conf) {}
+    static void measure(const Configuration& conf, std::size_t stamp) {}
     template <typename Configuration, typename Measure,
 	      typename... OtherMeasures>
-    static void measure(const Configuration& conf, Measure& m,
-		   OtherMeasures&... otherMeasures) {
-      m.measure(conf);
-      measure(conf, otherMeasures...);
+    static void measure(const Configuration& conf, std::size_t stamp,
+       Measure& m, OtherMeasures&... otherMeasures) {
+      m.measure(conf, stamp);
+      measure(conf, stamp, otherMeasures...);
     }
   };
 
