48770fe2da789b2ad8e6147f6e25f32f41f1b2d1
Yang Qi <yangqi137@me.com>
2016-12-08 18:54:30 -0500

    preparing magnetization for the new interface.

--- src/yqvmc/classical_ising/magnetization.hpp
+++ src/yqvmc/classical_ising/magnetization.hpp
@@ -3,12 +3,14 @@
 
 #include <algorithm>
 #include <cmath>
+#include <limits>
 
 namespace yqvmc { namespace ci {
   struct Magnetization {
     typedef double value_type;
+
     template <typename Conf>
-    value_type measure(const Conf& conf) {
+    value_type measure(const Conf& conf, std::size_t stamp = 0) {
       value_type m = 0.;
       for (bool s : conf) m += (s ? +1. : -1.);
       return std::abs(m / conf.size());
