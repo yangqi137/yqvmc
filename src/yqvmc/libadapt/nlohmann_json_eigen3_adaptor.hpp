8d4234045f2e21d76ba63d6a5d8ba751f747e9e7
Yang Qi <yangqi137@me.com>
2018-01-24 11:32:33 +0800

    wip

--- src/yqvmc/libadapt/nlohmann_json_eigen3_adaptor.hpp
+++ src/yqvmc/libadapt/nlohmann_json_eigen3_adaptor.hpp
@@ -9,7 +9,7 @@ namespace nlohmann {
     //};
     template <>
     struct adl_serializer<Eigen::ArrayXd> {
-      static void to_json(json& j, const Eigen::ArrayXd& vec) {
+      static void to_json(json& j, const Eigen::VectorXd& vec) {
         for (unsigned i = 0; i < vec.size(); i++) {
           j[i] = vec[i];
         }
@@ -22,11 +22,17 @@ namespace nlohmann {
         }
       }
 
-      static void to_json(json& j, const Eigen::ArrayXXd& vec) {
+      static void to_json(json& js, const Eigen::MatrixXd& mat) {
+        for (unsigned i = 0; i < mat.rows(); i++) {
+          js[i] = json();
+          for (unsigned j = 0; j < mat.cols(); j++) {
+            js[i][j] = mat(i, j);
+          }
+        }
       }
 
-      static void from_json(const json& j, Eigen::ArrayXXd& vec) {
-      }
+      //static void from_json(const json& j, Eigen::ArrayXXd& vec) {
+      //}
 
     };
 }
