366dce116a535fa450782fb0361951c1e36652cb
Yang Qi <qiyang@fudan.edu.cn>
2022-10-17 17:03:40 +0800

    adding support for std::valarray

--- src/yqvmc/impl_/mae_traits.hpp
+++ src/yqvmc/impl_/mae_traits.hpp
@@ -2,6 +2,7 @@
 #define YQVMC_IMPL_MEAN_AND_ERROR_TRAITS_HPP
 
 #include <cmath>
+#include <valarray>
 
 namespace yqvmc {
   namespace impl_ {
@@ -25,6 +26,30 @@ namespace yqvmc {
       }
       static result_type standard_error(result_type x2mean,
         result_type xmean, std::size_t n) {
+        return std::sqrt((x2mean - xmean*xmean) / n);
+      }
+    };
+
+    template <typename T>
+    struct MeanAndErrorTraits<std::valarray<T> > {
+      typedef T scalar_type;
+      typedef std::valarray<scalar_type> input_type;
+      typedef std::valarray<scalar_type> sum_type;
+      typedef std::valarray<scalar_type> result_type;
+
+      static void set_zero(sum_type& x) { 
+        if (x.size() != 0) x=0.;
+      }
+      static void add_to(sum_type& x, const input_type& dx) {
+        if (x.size() == 0) x = dx;
+        else x += dx;
+      }
+      static input_type square(const input_type& x) { return x*x; }
+      static result_type mean(const sum_type& x, std::size_t n) {
+        return x / n;
+      }
+      static result_type standard_error(const result_type& x2mean,
+        const result_type& xmean, std::size_t n) {
         return std::sqrt((x2mean - xmean*xmean) / n);
       }
     };
